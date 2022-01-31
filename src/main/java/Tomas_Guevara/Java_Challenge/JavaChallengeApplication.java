16628ad786a14764f24657558655239d0a0473b1
Tomas Guevara <tguevara428@gmail.com>
2022-01-31 16:16:45 -0300

    first commit, Maven project

---
+++ src/main/java/Tomas_Guevara/Java_Challenge/JavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package Tomas_Guevara.Java_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tomas_Guevara/Java_Challenge/JavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package Tomas_Guevara.Java_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
